002da598ad8a9fb89bc9023e2b2e2652903cb4d6
nickmyku <nickmyku@gmail.com>
2014-12-23 18:14:39 -0500

    things

--- beer.py
+++ beer.py
@@ -9,6 +9,20 @@ import RPi.GPIO as GPIO
 import pickle
 import Adafruit_ADS1x15
 
+# configure gpio pins
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
+ADS1115 = 0x01
+
+# set to the +/- 1.024V range
+gain = 1024
+
+# set the number of samples per second
+sps = 250
+
+# create an adc object
+adc = ADS1x15(ic=ADS1115)
+
+v_neg = 0
+v_pos = 0
